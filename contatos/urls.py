44089201288f2d9a9fbe84e786feaed89afecb4b
mark-sousa <mark--sousa1@hotmail.com>
2021-06-18 21:29:19 -0300

    adcionada busca simples

--- contatos/urls.py
+++ contatos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, mostrar_detalhes
+from .views import index, mostrar_detalhes, busca
 
 urlpatterns = [
     path('', index, name='url_index'),
+    path('busca/', busca, name='url_busca'),
     path('mostrar_detalhes/<int:pk>/', mostrar_detalhes, name='url_mostrar'),
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, Http404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
@@ -17,3 +19,20 @@ def mostrar_detalhes(request, pk):
         raise Http404()
 
     return render(request, 'contatos/detalhes.html', {'detalhe': detalhe})
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+    if termo is None:
+        raise Http404
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo))
+    print(contatos.query)
+    paginator = Paginator(contatos, 5)
+    page_number = request.GET.get('page')
+    contatos = paginator.get_page(page_number)
+    return render(request, 'contatos/index.html', {'contatos': contatos})
